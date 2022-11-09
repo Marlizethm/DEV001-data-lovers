bc7fe36beb01bb9a76e02b9da7827aaf6e5b293b
Marlizethm <114497605+Marlizethm@users.noreply.github.com>
2022-11-09 11:21:39 -0600

    actualización menu

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
 import ghibli from "./data/ghibli/ghibli.js";
 
-// console.log(ghibli);
-const btnAbrirIntro = document.querySelector('#btn-abrir-intro'),  /*Estoy llamando el ID del botón para abrir la caja de mi intro*/
-    introduccion = document.querySelector('#intro');
-
-btnAbrirIntro.addEventListener('click', () => {   /*Evento clic para luego pasarle una función de tipo flecha para que ejecute la parte de abrir la caja de la intro y otro para girar el botón*/
-// import data from './data/data.js';
-    btnAbrirIntro.classList.toggle('active');   /*Necesito acceder al botón, luego que acceda a su lista de clases y luego que tenga un toggle de la clase active*/
-    introduccion.classList.toggle('active');
-} );
-
-
 let films = ghibli.films;
 // console.log(films);
-const peliculas = document.getElementById("peliculas");
-for (let i = 0; i < films.length; i++){
+const peliculas = document.getElementById("tarjetas");
+for (let i = 0; i < films.length; i++) {
     // console.log(films[i].title);
     // console.log(films[i].poster);
     peliculas.innerHTML += `
@@ -22,11 +11,28 @@ for (let i = 0; i < films.length; i++){
     <img src="${films[i].poster}" alt"">
 
     <div class = "textos_portada">
-    <h2>${films[i].title}</h2>
-    <p>${films[i].release_date}</p>
-    </div>
-    </div>
-    `
+        <h2>${films[i].title}</h2>
+        <p>${films[i].release_date}</p>
+        <h2>${films[i].director}</h2>
+        </div>
+
+        <div class = "boton_modal">
+        <a href="#" class="botonModal">+</a>
+        </div>
+    </div>`
 }
 
 //<p>${films[i].description}</p>
+/* 
+films = ghibli.films;
+const directores = document.getElementById(Hayao Miyazaki);
+for(let i = 0; i < films.length; i++){
+    directores.innerHTML=`
+    <div class = "d1">
+    <
+    `
+} */
+
+
+
+
