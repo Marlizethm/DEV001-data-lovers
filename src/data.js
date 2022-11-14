4f39251ed779b31c7823c8d4a6502dbb53180adc
Valapvil26 <valentinaapontevillamil@gmail.com>
2022-11-14 18:27:25 -0500

    Actualización filter

--- src/data.js
+++ src/data.js
@@ -7,20 +7,8 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
-/* 
-export const newsest = (data) => {
-let newsestmovies
-for(let i=0; i < data.length; i++){
-newsestmovies = data.sort(function(a,b){
-  if(a.release_date == b.release_date){
-    return 0;
-  }
-  if(a.release_date > b.release_date){
-    return -1;
-  }
-    return 1;
-});
 
-  return newsestmovies;
-}; */
-
+// export const filterDir = (data, director) => {
+//   const filtrarDirector = data.filter (data => data.director === director);{
+//     return filtrarDirector;
+//   }};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import data from "./data/ghibli/ghibli.js";
 import ghibli from "./data/ghibli/ghibli.js";
+// import {filterDir} from "./data.js";
 
-let films = ghibli.films;
+
+
+let films = data.films;
 // console.log(films);
+
 const peliculas = document.getElementById("tarjetas");
 for (let i = 0; i < films.length; i++) {
     // console.log(films[i].title);
@@ -9,30 +14,48 @@ for (let i = 0; i < films.length; i++) {
     peliculas.innerHTML += `
     <div class = "portada">
     <img src="${films[i].poster}" alt"">
+    <div class = "boton_modal">
+        <a href="#" class="botonModal">+</a>
+        </div>
 
     <div class = "textos_portada">
         <h2>${films[i].title}</h2>
         <p>${films[i].release_date}</p>
         <h2>${films[i].director}</h2>
         </div>
-
-        <div class = "boton_modal">
-        <a href="#" class="botonModal">+</a>
-        </div>
     </div>`
 }
 
-//<p>${films[i].description}</p>
-/* 
-films = ghibli.films;
-const directores = document.getElementById(Hayao Miyazaki);
-for(let i = 0; i < films.length; i++){
-    directores.innerHTML=`
-    <div class = "d1">
-    <
-    `
-} */
+const botonHayao = document.getElementById("Hayao-Miyazaki");
+// const botonHiroyuki = document.getElementById("Hiroyuki-Morita");
+// const botonGorō = document.getElementById("Gorō-Miyazaki");
+// const botonYoshifumi = document.getElementById("Yoshifumi-Kondō");
+// const botonHiromasa = document.getElementById("Hiromasa-Yonebayashi");
+// const botonIsao = document.getElementById("Isao-Takahata");
+const contenedor = document.getElementById("tarjetas");
+const limpiarContenedor = () => {
+  contenedor.innerHTML = "";
+  };
+
+const directores = ghibli.films;
+
+const filterDirector = directores.filter ((films) => {
+return films.director === "Hayao Miyazaki";
+});
+
+botonHayao.addEventListener ("click", () => {
+limpiarContenedor ();
+filterDirector.forEach ((films) => {
+contenedor.innerHTML += `
+<h2> ${films.director}</h2}`;
+});
+});
 
 
 
+// const filterDirector = document.getElementById("filter-director");
+// filterDirector.addEventListener("change", () => {
+//   let newArray = directorGhibli;
+//   show (filterDir(directorGhibli.films, filterDirector.value));
+// });
 
